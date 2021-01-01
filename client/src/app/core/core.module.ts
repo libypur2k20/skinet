a694a0784881791a6791491dc588b992c2a7b066
unknown <github@conocelasolucion.es>
2021-01-01 15:20:02 +0100

    End of Section 11

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule  // Para poder agregar atributos router a los links de navegación del nav-bar component.
+    RouterModule,  // Para poder agregar atributos router a los links de navegación del nav-bar component.
+    ToastrModule.forRoot(
+      {
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true
+      }
+    )
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent, TestErrorComponent]
 })
 export class CoreModule { }
